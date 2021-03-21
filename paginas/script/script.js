d9beeabc3ec9601480f911c26cb14f40278b81fb
cri34 <cristiangs777@gmail.com>
2021-03-21 17:42:47 +0100

    Commit Inicial

---
+++ paginas/script/script.js
@@ -0,0 +1,10 @@
+// guardamos el id de la imagen visible
+idImagenVisible = "Fotografía Burbu";
+function mostrar(id) {
+    // esconedemos la imagen visible
+    document.getElementById(idImagenVisible).style.display = "none";
+    // mostramos la imagen seleccionada
+    document.getElementById(id).style.display = "block";
+    // guardamos en la variable la imagen que estamos visualizando
+    idImagenVisible = id;
+}
